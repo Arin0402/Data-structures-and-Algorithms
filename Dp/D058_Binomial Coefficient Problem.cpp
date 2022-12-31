8ab487fedff577855e1b8321d6b60520588a2f67
Arin0402 <goar045@gmail.com>
2022-12-31 15:50:09 +0530

    added

---
+++ Dp/D058_Binomial Coefficient Problem.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://practice.geeksforgeeks.org/problems/ncr1019/1
+// https://www.youtube.com/watch?v=jIb1W3ObIho
+
+// 1
+// o(r*r);
+// O(r)
+
+#define MOD 1000000007
+
+class Solution
+{
+public:
+    int nCr(int n, int r)
+    {
+
+        if (n < r)
+            return 0;
+        if (n - r < r)
+            r = n - r;
+
+        vector<int> dp(r + 1, 0);
+
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            int ind = r;
+
+            while (ind > 0)
+            {
+                dp[ind] = (dp[ind] + dp[ind - 1]) % MOD;
+                ind--;
+            }
+        }
+
+        return dp[r];
+    }
+};
