6216c6ba32da0e89916c908220a431258f73a8af
Arin0402 <goar045@gmail.com>
2023-01-03 00:42:36 +0530

    added

---
+++ Dp/D069_Maximum subsequence sum such that no three are consecutive.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://practice.geeksforgeeks.org/problems/max-sum-without-adjacents-2/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+// 1
+// memo
+// O(3*N)
+// O(3*N) + O(N)
+class Solution
+{
+public:
+    // keep a counter which will denote consecutiveness.
+    int helper(int ind, int count, vector<int> &a, vector<vector<int>> &dp)
+    {
+
+        if (ind < 0)
+            return 0;
+
+        if (dp[ind][count] != -1)
+            return dp[ind][count];
+
+        // for not_take condition, reset counter to zero.
+        int not_take = helper(ind - 1, 0, a, dp);
+
+        int take = 0;
+
+        // if count is 2 , then it means we have already taken two consecutive elements
+        // ans we can not take this one.
+        if (count < 2)
+            take = a[ind] + helper(ind - 1, count + 1, a, dp);
+
+        return dp[ind][count] = max(take, not_take);
+    }
+    int findMaxSum(vector<int> &a)
+    {
+
+        int n = a.size();
+
+        vector<vector<int>> dp(n, vector<int>(3, -1));
+        return helper(n - 1, 0, a, dp);
+    }
+};
+
+// OR
+
+class Solution
+{
+public:
+    // keep a counter which will denote consecutiveness.
+    int helper(int ind, int count, vector<int> &a, vector<vector<int>> &dp)
+    {
+
+        if (ind == 0)
+            return 0;
+
+        if (dp[ind][count] != -1)
+            return dp[ind][count];
+
+        // for not_take condition, reset counter to zero.
+        int not_take = helper(ind - 1, 0, a, dp);
+
+        int take = 0;
+
+        // if count is 2 , then it means we have already taken two consecutive elements
+        // ans we can not take this one.
+        if (count < 2)
+            take = a[ind - 1] + helper(ind - 1, count + 1, a, dp);
+
+        return dp[ind][count] = max(take, not_take);
+    }
+    int findMaxSum(vector<int> &a)
+    {
+
+        int n = a.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(3, -1));
+        return helper(n, 0, a, dp);
+    }
+};
+
+// 2
+// tab
+class Solution
+{
+public:
+    // keep a counter which will denote consecutiveness.
+    int findMaxSum(vector<int> &a)
+    {
+
+        int n = a.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(3, 0));
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+            {
+
+                int not_take = dp[i - 1][0];
+                int take = 0;
+
+                if (j < 2)
+                    take = a[i - 1] + dp[i - 1][j + 1];
+
+                dp[i][j] = max(take, not_take);
+            }
+        }
+
+        return dp[n][0];
+    }
+};
+
+// 3
+// space optimised
+class Solution
+{
+public:
+    // keep a counter which will denote consecutiveness.
+    int findMaxSum(vector<int> &a)
+    {
+
+        int n = a.size();
+
+        vector<int> prev(3, 0);
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            vector<int> temp(3, 0);
+            for (int j = 0; j <= 2; j++)
+            {
+
+                int not_take = prev[0];
+                int take = 0;
+
+                if (j < 2)
+                    take = a[i - 1] + prev[j + 1];
+
+                temp[j] = max(take, not_take);
+            }
+
+            prev = temp;
+        }
+
+        return prev[0];
+    }
+};
---
+++ Dp/D070_Egg dropping problem.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle-1587115620/1
+// https://www.youtube.com/watch?v=S49zeUjeUL0.
+// Reason for maximum - watch pepcoding video
+
+// 1
+// memo
+// O(N*(K^2))
+// O(N*K) + O(n + k)
+class Solution
+{
+public:
+    // Function to find minimum number of attempts needed in
+    // order to find the critical floor.
+    int helper(int eggs, int floors, vector<vector<int>> &dp)
+    {
+
+        if (floors == 0 || floors == 1)
+            return floors;
+        if (eggs == 1)
+            return floors;
+
+        if (dp[eggs][floors] != -1)
+            return dp[eggs][floors];
+
+        int mini = INT_MAX;
+
+        for (int k = 1; k <= floors; k++)
+        {
+
+            int not_broke = 1 + helper(eggs, floors - k, dp);
+            int broke = 1 + helper(eggs - 1, k - 1, dp);
+
+            int temp = max(not_broke, broke);
+
+            mini = min(mini, temp);
+        }
+
+        return dp[eggs][floors] = mini;
+    }
+
+    int eggDrop(int n, int k)
+    {
+
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, -1));
+        return helper(n, k, dp);
+    }
+};
+
+// 2
+// tab
+// O(N*(K^2))
+// O(N*K)
+class Solution
+{
+public:
+    int eggDrop(int n, int k)
+    {
+
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][1] = 1;
+        }
+
+        for (int j = 0; j <= k; j++)
+            dp[1][j] = j;
+
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 2; j <= k; j++)
+            {
+
+                int mini = INT_MAX;
+
+                for (int x = 1; x <= j; x++)
+                {
+
+                    int not_broke = 1 + dp[i][j - x];
+                    int broke = 1 + dp[i - 1][x - 1];
+
+                    int temp = max(not_broke, broke);
+
+                    mini = min(mini, temp);
+                }
+
+                dp[i][j] = mini;
+            }
+        }
+
+        return dp[n][k];
+    }
+};
