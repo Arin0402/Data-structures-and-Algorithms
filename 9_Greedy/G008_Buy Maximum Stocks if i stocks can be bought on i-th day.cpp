ab6a79f020371f160ba6a309bfeb04e160020031
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-08 19:41:06 +0530

    Update G008_Buy Maximum Stocks if i stocks can be bought on i-th day.cpp

--- 9_Greedy/G008_Buy Maximum Stocks if i stocks can be bought on i-th day.cpp
+++ 9_Greedy/G008_Buy Maximum Stocks if i stocks can be bought on i-th day.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// https://www.geeksforgeeks.org/problems/buy-maximum-stocks-if-i-stocks-can-be-bought-on-i-th-day/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
 class Solution
 {
 public:
