e6d6308b6ed704415c973aa0e6a945001e6b39a3
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-09 12:20:49 +0530

    Update G009_Find the minimum and maximum amount to buy all N candies.cpp

--- 9_Greedy/G009_Find the minimum and maximum amount to buy all N candies.cpp
+++ 9_Greedy/G009_Find the minimum and maximum amount to buy all N candies.cpp
@@ -1,3 +1,5 @@
+// https://www.geeksforgeeks.org/problems/shop-in-candy-store1145/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
 #include <bits/stdc++.h>
 using namespace std;
 
