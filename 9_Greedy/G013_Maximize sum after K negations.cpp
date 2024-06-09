9f68fe48688a6c5cd1ec692714e4c13e788ee7b5
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-09 18:24:09 +0530

    Update G013_Maximize sum after K negations.cpp

--- 9_Greedy/G013_Maximize sum after K negations.cpp
+++ 9_Greedy/G013_Maximize sum after K negations.cpp
@@ -1,73 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
+
 // 1
 // O(nlogn)
-class Solution
-{
+class Solution {
 public:
-    long long int total(int ind, long long int a[], int n, int k)
-    {
+    int largestSumAfterKNegations(vector<int>& a, int k) {
+        int countNegatives = 0;
+        int n = a.size();
 
-        long long int sum = 0;
-
-        for (int i = ind; i < n; i++)
-            sum += a[i];
-
-        return sum;
-    }
-
-    long long int maximizeSum(long long int a[], int n, int k)
-    {
-
-        sort(a, a + n);
-
-        int i = 0;
-
-        long long int sum = 0;
-
-        for (int i = 0; i < n; i++)
-        {
-
-            // current element is positive so the remaining will also be positive.
-            if (a[i] >= 0)
-            {
-
-                // we have to spend all the operations on this current element.
-                if (k % 2 == 0)
-                    sum += total(i, a, n, k);
-                else
-                    sum += -1 * a[i] + total(i + 1, a, n, k);
-
-                break;
-            }
-            // last element of the array and operations are still left.
-            // so we have to spend all the operations on this current element.
-            else if (i == n - 1)
-            {
-
-                if (k % 2 == 0)
-                    sum += a[i];
-                else
-                    sum += -1 * a[i];
-            }
-            // negative element.
-            else
-            {
-
-                // operations are left so make element positive.
-                if (k > 0)
-                {
-                    sum += -1 * a[i];
-                    k--;
-                }
-
-                // operations are over , so add the remaining elements.
-                else
-                    sum += a[i];
-            }
+        for(int i = 0; i < n; i++){
+            if(a[i] < 0) countNegatives++;
         }
-
+        
+        int sum = 0;
+        
+        
+        // all are positive
+        if(countNegatives == 0 || k == 0){
+            
+            // k is even
+            if(k%2 == 0){
+                for(int i = 0; i < n; i++) sum += a[i];
+            }
+            
+            // k is odd, consume all on the first element
+            else{
+                
+                sort(a.begin(), a.end());
+                
+                sum += -1*a[0];
+                for(int i = 1; i < n; i++) sum += a[i];
+            }
+        } 
+        else{
+            
+            // both are equal
+            if(countNegatives == k){
+                for(int i = 0; i < n; i++) {
+                    if(a[i] < 0){
+                        a[i] = -1*a[i];
+                        k--;
+                    }
+                    
+                    sum += a[i];                
+                }
+            }
+            
+            // we have more negatives than k
+            else if(countNegatives > k){
+                
+                sort(a.begin(), a.end());
+                
+                int i = 0;
+                for(; i < k; i++) sum += -1*a[i];
+                
+                for(; i < n; i++) sum += a[i];
+                
+            }
+            // we have less negatives than k
+            else{
+                
+                for(int i = 0; i < n; i++) {
+                    if(a[i] < 0){
+                        a[i] = -1*a[i];
+                        k--;
+                    }
+                    
+                    sum += a[i];
+                    
+                }
+                 
+                sort(a.begin(), a.end());
+                
+                 
+                if(k%2 != 0){
+                    
+                    sum -= a[0];
+                    sum += -1*a[0];
+                } 
+                
+            }
+            
+        }
+        
         return sum;
     }
 };
