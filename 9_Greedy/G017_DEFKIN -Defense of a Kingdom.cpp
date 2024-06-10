deb9b8c8606011e5093a2e370aa8976e90fc120e
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-10 10:31:26 +0530

    Update G017_DEFKIN -Defense of a Kingdom.cpp

--- 9_Greedy/G017_DEFKIN -Defense of a Kingdom.cpp
+++ 9_Greedy/G017_DEFKIN -Defense of a Kingdom.cpp
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// https://www.spoj.com/problems/DEFKIN/
 // https://www.youtube.com/watch?v=fk1yIirivEo
 // very easy.
+
+// we have to find the maximum distance between the towers on both x coordinate and y coordinates.
+// the answer would be the multiplication of the two.
