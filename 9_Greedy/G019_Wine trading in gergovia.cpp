a251873d68de751e2d2b124019f339ed40d4706d
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-10 11:50:18 +0530

    Update G019_Wine trading in gergovia.cpp

--- 9_Greedy/G019_Wine trading in gergovia.cpp
+++ 9_Greedy/G019_Wine trading in gergovia.cpp
@@ -2,77 +2,66 @@
 using namespace std;
 
 // https://www.geeksforgeeks.org/problems/wine-buying-and-selling/1
-
+// https://www.youtube.com/watch?v=ggLUc8HHFvQ
 // person will purchase the item from the shop which is closest possible.
-int main()
-{
+// two pointer approach
 
-	while (true)
-	{
+class Solution{
+    public:
+    long long int wineSelling(vector<int>& arr, int n){
+      
+        long long int ans = 0;
+        
+        int buy = 0;
+        int sell = 0;
+        
+        // find the first buyer index
+        for(int i =0; i < n; i++) {
+            if(arr[i] > 0){
+                buy = i;
+                break;
+            }
+        }
+        
+        // find the first seller index
+        for(int i = 0; i < n; i++){
+            if(arr[i] < 0){
+                sell = i;
+                break;
+            }
+        }
+        
+        while(buy < n && sell < n){
+            
+            // calculate distance between them
+            int distance = abs(buy - sell);
+            
+            // buyer wants more or equal
+            if(arr[buy] >= abs(arr[sell])){
+                
+                ans += abs(arr[sell])*distance;
+                
+                arr[buy] -= abs(arr[sell]);
+                arr[sell] = 0;
+                
+            }
+            else{
+                ans += arr[buy]*distance;
+                
+                arr[sell] += arr[buy];
+                arr[buy] = 0;
+              
+            }
+            
+            // move to next buyer and seller if requirement is fulfilled.
+            while(buy < n && arr[buy] <= 0) buy++;
+            while(sell < n && arr[sell] >= 0) sell++;
+               
+        }
+        
+        return ans;
+        
+      
+    }
+};
 
-		int n;
-		cin >> n;
-
-		if (n == 0)
-			break;
-
-		// Note - it can solved without using extra space for positives and negatives.
-		// just use two poinetrs for buy and sell.
-		// if need intution , anuj bhaiya
-
-		vector<pair<int, int>> positives; // all buy .
-		vector<pair<int, int>> negatives; // all sell.
-
-		for (int i = 0; i < n; i++)
-		{
-			int x;
-			cin >> x;
-
-			if (x >= 0)
-				positives.push_back({x, i});
-			else
-				negatives.push_back({-1 * x, i});
-		}
-
-		int p1 = 0;
-		int p2 = 0;
-
-		long long int ans = 0;
-
-		// Two pointer approach.
-		while (p1 < positives.size() && p2 < negatives.size())
-		{
-
-			int buy = positives[p1].first;
-			int sell = negatives[p2].first;
-
-			int buyInd = positives[p1].second;
-			int sellInd = negatives[p2].second;
-
-			if (buy == sell)
-			{
-				ans += buy * (abs((buyInd - sellInd)));
-				p1++;
-				p2++;
-			}
-			else if (buy > sell)
-			{
-
-				positives[p1].first -= sell;			 // decrease the demand.
-				ans += sell * (abs((buyInd - sellInd))); // add to the answer.
-				p2++;
-			}
-			else
-			{
-
-				negatives[p2].first -= buy;
-				ans += buy * (abs((buyInd - sellInd)));
-				p1++;
-			}
-		}
-
-		cout << ans << endl;
-	}
-
-	return 0;
-}
