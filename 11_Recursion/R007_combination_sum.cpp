c1f760efdc2160ea5158d662f1e2ebc43d973c3c
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-13 15:06:37 +0530

    Update R007_combination_sum.cpp

--- 11_Recursion/R007_combination_sum.cpp
+++ 11_Recursion/R007_combination_sum.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // Reason: Assume if you were not allowed to pick a single element multiple times, every element will have a couple of options: pick or not pick which is 2^n different recursion calls, also assuming that the average length of every combination generated is k. (to put length k data structure into another data structure)
 // Why not (2^n) but (2^t) (where n is the size of an array)?
 // Assume that there is 1 and the target you want to reach is 10 so 10 times you can “pick or not pick” an element.
+// reason for k -> we are pushing the ds vector into the ans vector whcih takes time
 
 class Solution
 {
