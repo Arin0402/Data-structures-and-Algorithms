971fd62500ee7a843ceed7dce473febfcb565d14
Arin Goyal <83952361+Arin0402@users.noreply.github.com>
2024-06-20 19:03:31 +0530

    Update D017_Count subsets with sum k.cpp

--- 14_Dp/D017_Count subsets with sum k.cpp
+++ 14_Dp/D017_Count subsets with sum k.cpp
@@ -47,3 +47,44 @@ public:
 		return helper(n - 1, sum, arr, dp);
 	}
 };
+
+// 2
+// memo
+// O(n*target)
+	#define MOD 1000000007
+class Solution{
+
+	int perfectSum(int arr[], int n, int target)
+	{
+
+		vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+		
+		
+		if(arr[0] == 0) dp[0][0] = 2;
+		else{
+		    dp[0][0] = 1;
+		    if(arr[0] <= target) dp[0][arr[0]] = 1;
+		    
+		}
+		
+		
+		for(int i = 1; i < n; i++){
+		    for(int j = 0; j <= target; j++){ 
+		        
+        		int not_take = dp[i - 1][j];
+        		int take = 0;
+        
+        		if (arr[i] <= j)
+        			take = dp[i - 1][j - arr[i]];
+        
+        		dp[i][j] = (not_take % MOD + take % MOD) % MOD;   
+		    }
+		}
+		
+
+		return dp[n - 1][target];
+
+
+	}
+	  
+};
